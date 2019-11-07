3fa59cb5e7eea38a5eeda08907ef6ae4267e10f0
고태건 <gtg7784@naver.com>
2019-11-07 11:58:13 +0900

    update

--- src/containers/Inquiry/Inquiry.js
+++ src/containers/Inquiry/Inquiry.js
@@ -3,13 +3,21 @@ import * as classnames from 'classnames/bind'
 
 import styles from './Inquiry.scss'
 
+import i_box from 'assets/box.svg'
+
 const cx = classnames.bind(styles)
 
 class Inquiry extends React.Component{
   render(){
     return(
       <div className={cx('Inquiry')}>
-
+        <div>
+          <h1>택배조회</h1>
+          <hr/>
+          <img src={i_box} alt=""/>
+          <p>운송장번호를 입력하시면 배송상태를 확인하실 수 있습니다.</p>
+          <input type="text" />
+        </div>
       </div>
     )
   }
